3b5850b774bb9898cfc18db7d71c2521e137d6ec
ThiagoTeixeiraa <thiagosousateixeiraa@gmail.com>
2019-09-14 12:48:50 -0300

    Add To Cart

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
-import { formatPrice } from '../../util/format'
+import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-
-export default class Home extends Component {
+class Home extends Component {
   state = {
-    products:[]
-  }
+    products: [],
+  };
 
-  async componentDidMount(){
+  async componentDidMount() {
     const response = await api.get('products');
 
     const data = response.data.map(product => ({
       ...product,
-      formatedPrice:formatPrice(product.price),
-    }))
+      formatedPrice: formatPrice(product.price),
+    }));
 
     this.setState({ products: data });
   }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
 
   render() {
     const { products } = this.state;
 
     return (
       <ProductList>
-        { products.map(product => (
-          <li key={ product.id }>
-            <img src={ product.image } alt={ product.title } />
-            <strong>{ product.title }</strong>
-            <span>{ product.formatedPrice }</span>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.formatedPrice}</span>
 
-            <button type="button">
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product)}
+            >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -41,9 +53,10 @@ export default class Home extends Component {
               <span>ADICIONAR AO CARRINHO</span>
             </button>
           </li>
-        )) }
-
+        ))}
       </ProductList>
     );
   }
 }
+
+export default connect()(Home);
